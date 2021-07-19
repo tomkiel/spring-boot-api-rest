c626afa878c72d934d0ffaee2bfd355ade8fd2ae
tomkiel <regis@doseextra.com>
2021-07-19 15:32:33 -0300

    Queries customizadas para consulta no banco de dados

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.alura.forum.modelo.Topico;
 
@@ -11,5 +13,8 @@ public interface TopicoRepository extends JpaRepository<Topico, Long> {
 	List<Topico> findByTitulo(String titulo);
 	
 	List<Topico> findByCursoNome(String nomeCurso);
+	
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+	List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
 	
 }
